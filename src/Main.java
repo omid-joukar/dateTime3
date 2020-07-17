65e33324018c5f5268e621c3fc9a6c99a82906c3
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-17 12:24:12 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Calendar;
+
+/**
+ * Created by KPS on 7/17/2020.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        // Create a default calendar
+        Calendar cal = Calendar.getInstance();
+        System.out.println();
+        System.out.println("\nCurrent Date and Time:" + cal.getTime());
+        int actualMaxYear = cal.getActualMaximum(Calendar.YEAR);
+        int actualMaxMonth = cal.getActualMaximum(Calendar.MONTH);
+        int actualMaxWeek = cal.getActualMaximum(Calendar.WEEK_OF_YEAR);
+        int actualMaxDate = cal.getActualMaximum(Calendar.DATE);
+
+        System.out.println("Actual Maximum Year: "+actualMaxYear);
+        System.out.println("Actual Maximum Month: "+actualMaxMonth);
+        System.out.println("Actual Maximum Week of Year: "+actualMaxWeek);
+        System.out.println("Actual Maximum Date: "+actualMaxDate+"\n");
+        System.out.println();
+
+    }
+}
